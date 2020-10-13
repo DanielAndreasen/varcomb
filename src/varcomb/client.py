f434e68b6e06661b21ca73f2f2f54351be39e55a
Daniel T. Andreasen <daniel.andreasen@hotmail.com>
2020-10-13 13:07:08 +0200

    Add annotation to client

--- src/varcomb/client.py
+++ src/varcomb/client.py
@@ -9,7 +9,7 @@ from varcomb.utilities import read_vcf
 @click.group()
 @click.option('--log_file')
 @click.pass_context
-def client(cts, log_file, type=click.Path()):
+def client(ctx, log_file, type=click.Path()):
     logging.basicConfig(format='[%(levelname)s] %(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO, filename=log_file)
 
 
@@ -17,12 +17,18 @@ def client(cts, log_file, type=click.Path()):
 @click.option('--vcf_file1', required=True, type=click.Path())
 @click.option('--vcf_file2', required=True, type=click.Path())
 @click.option('--vcf_out', required=True, type=click.Path())
+@click.option('--ann_vcf1', required=False)
+@click.option('--ann_vcf2', required=False)
 @click.pass_context
-def merge_vcfs(ctx, vcf_file1, vcf_file2, vcf_out):
+def merge_vcfs(ctx, vcf_file1, vcf_file2, vcf_out, ann_vcf1=None, ann_vcf2=None):
     logging.info(f'Reading file: {vcf_file1}')
     vcf1 = parse_vcf_file(read_vcf(vcf_file1))
     logging.info(f'Reading file: {vcf_file2}')
     vcf2 = parse_vcf_file(read_vcf(vcf_file2))
+    if ann_vcf1 is not None:
+        vcf1 = vcf1.annotate(ann_vcf1)
+    if ann_vcf2 is not None:
+        vcf2 = vcf2.annotate(ann_vcf2)
     vcf = vcf1 + vcf2
     vcf = vcf.remove_true_duplicates()
     vcf.to_file(vcf_out)
