2e19717703d3c07665c039f4c8a9d567431f8e10
Daniel T. Andreasen <daniel.andreasen@hotmail.com>
2020-10-13 13:23:03 +0200

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
 
     name='varcomb',
-    version='0.0.1',
+    version='0.0.2',
 
     packages=find_packages('src'),
     package_dir={'': 'src'},
