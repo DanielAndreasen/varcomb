8a27a7ce87182698cde1cb996eeb1203a907c03e
Daniel T. Andreasen <daniel.andreasen@hotmail.com>
2020-10-14 11:15:23 +0200

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
 
     name='varcomb',
-    version='0.0.2',
+    version='0.0.3',
 
     packages=find_packages('src'),
     package_dir={'': 'src'},
